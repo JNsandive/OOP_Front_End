ed599d7de364705ae9d5948aa3c23ac269a5a63d
Nihindu <nihiyauj@gmail.com>
2024-12-02 21:04:49 +0530

    sales dash board updated with integrating websocket and vendor essentials developed

---
+++ src/app/edit-configuration/edit-configuration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-configuration',
+  imports: [],
+  templateUrl: './edit-configuration.component.html',
+  styleUrl: './edit-configuration.component.css'
+})
+export class EditConfigurationComponent {
+
+}
